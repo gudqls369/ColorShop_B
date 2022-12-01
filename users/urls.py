a6d0f3a376bc60a704fb80e998862ce458cedfd5
0sol0 <jdsmy1004@gmail.com>
2022-12-01 23:25:49 +0900

    ✨ 프로필 기능

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.UserView.as_view(), name='user_view'),
     path('api/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('<int:user_id>/', views.ProfileView.as_view(), name='profile_view')
     ]
