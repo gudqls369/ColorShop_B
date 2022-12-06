bec406014a2f62e619359f586c1166d55d09ef28
tweaky3047 <yy304742@gmail.com>
2022-12-06 20:07:31 +0900

    :horse: Fix:유저회원가입시 기능 유효성검사하도록 수정

--- ColorShop_B/settings.py
+++ ColorShop_B/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    'image_optimizer',
     'rest_framework',
     'rest_framework_simplejwt',
     "corsheaders",
@@ -35,9 +36,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+ 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     "corsheaders.middleware.CorsPostCsrfMiddleware",
