c356e83895286a616027ff196cbcf538dcfe1196
tweaky3047 <yy304742@gmail.com>
2022-12-06 20:07:31 +0900

    :horse: Fix:유저회원가입시 기능 유효성검사하도록 수정

--- users/models.py
+++ users/models.py
@@ -4,24 +4,25 @@ from django.contrib.auth.models import (
 )
 from image_optimizer.fields import OptimizedImageField
 
-# Create your models here.
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None):
+    def create_user(self, username, nickname, password=None):
         if not username:
             raise ValueError('Users must have a username')
 
         user = self.model(
             username=username,
+            nickname=nickname,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None):
+    def create_superuser(self, username, nickname, password=None):
         user = self.create_user(
             username,
-            password=password
+            nickname=nickname,
+            password=password,
         )
 
         user.is_admin = True
@@ -49,7 +50,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['nickname',]
 
     def __str__(self):
         return self.username
